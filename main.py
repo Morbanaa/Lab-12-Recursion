3f7d8441a339ead81dd5422895f9da59e73e9bf8
Morbanaa <theodorerodd@gmail.com>
2026-05-12 13:36:06 -0500

    Multiplication function get x and y from user and using recursion gets y + y x number of times

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 #Lab 12 Recursion
 
 def main():
-    counter = 1
+
+    ############################################
+    # Question One: Recursive Printing
+    ############################################
     print("Question One: Recursive Printing")
     while True:
+        counter = 1
         try:
             value1 = int(input("Enter a positive number greater than 1: "))
             if value1 < 2:
@@ -14,11 +18,44 @@ def main():
             break
         except ValueError:
             print("Must be a number!")
-
+    
+    print(f"\nValues 1 - {value1}:")
     recursive_printing(value1,counter)
 
+
+    ############################################
+    # Question Two: Recursive Multiplication
+    ############################################
+
     print("\nQuestion Two: Recursive Multiplication")
-    recursive_multiplication()
+    counter = 0
+    total = 0
+    while True:
+        try:
+            x = int(input("Enter a positive non zero integer for X: "))
+            if x < 1:
+                print("X must be greater than zero!")
+                continue
+            break
+        except ValueError:
+            print("Value Error! X must be a positive non zero integer!")
+
+    while True:
+        try:
+            y = int(input("Enter a positive non zero integer for Y: "))
+            if y < 1:
+                print("Y must be greater than zero!")
+                continue
+            break
+        except ValueError:
+            print("Value Error! Y must be a postive non zero integer!")
+
+    print(f"\nTotal {x} X {y} = {recursive_multiplication(x,y,counter,total)}")
+
+
+    ############################################
+    # Question Three: Recursive Lines
+    ############################################
 
     print("\nQuestion Three: Recursive Lines")
     recursive_lines()
@@ -32,8 +69,11 @@ def recursive_printing(number,counter):
         return recursive_printing(number,counter + 1)
 
 
-def recursive_multiplication():
-    pass
+def recursive_multiplication(x,y,counter,total):
+    if counter == x:
+        return total
+    else:
+        return recursive_multiplication(x,y,counter + 1,total + y)
 
 
 def recursive_lines():
