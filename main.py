47a1889a13788d5eff85c812299a17e1b51e396f
Morbanaa <theodorerodd@gmail.com>
2026-05-12 13:05:53 -0500

    Main collects user input accoutning for negitive numbers and value errors. Using recursion it counts from 1 to the given number

--- main.py
+++ main.py
@@ -3,8 +3,19 @@
 #Lab 12 Recursion
 
 def main():
+    counter = 1
     print("Question One: Recursive Printing")
-    recursive_printing()
+    while True:
+        try:
+            value1 = int(input("Enter a positive number greater than 1: "))
+            if value1 < 2:
+                print("The number must be greater than 1!")
+                continue
+            break
+        except ValueError:
+            print("Must be a number!")
+
+    recursive_printing(value1,counter)
 
     print("\nQuestion Two: Recursive Multiplication")
     recursive_multiplication()
@@ -13,14 +24,21 @@ def main():
     recursive_lines()
     print()
 
-def recursive_printing():
-    pass
+def recursive_printing(number,counter):
+    if counter == number + 1:
+        return
+    else:
+        print(counter)
+        return recursive_printing(number,counter + 1)
+
 
 def recursive_multiplication():
     pass
 
+
 def recursive_lines():
     pass
+
 
 if __name__ == "__main__":
     main()
