5faac541913e3ab01b65a938927a815d221233c8
Morbanaa <theodorerodd@gmail.com>
2026-05-12 12:23:49 -0500

    Set up main function and functions for each question in the lab

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-ECHO is on.
+#Teddy Rodd
+#Morbanaa Studios
+#Lab 12 Recursion
+
+def main():
+    print("Question One: Recursive Printing")
+    recursive_printing()
+
+    print("\nQuestion Two: Recursive Multiplication")
+    recursive_multiplication()
+
+    print("\nQuestion Three: Recursive Lines")
+    recursive_lines()
+    print()
+
+def recursive_printing():
+    pass
+
+def recursive_multiplication():
+    pass
+
+def recursive_lines():
+    pass
+
+if __name__ == "__main__":
+    main()
